038a8efc283c8a50ca497bd5845f6fc6e740ed30
It3witch <xin2005629@163.com>
2025-10-12 15:27:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -72,15 +72,16 @@ const BLOG = {
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
     'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
-
+  
   // uuid重定向至 slug
-  UUID_REDIRECT: process.env.UUID_REDIRECT || false
-
+  UUID_REDIRECT: process.env.UUID_REDIRECT || false,
+  
   // 鼠标点击烟花特效
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
+  
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
   FIREWORKS_COLOR: ['255, 20, 97', '24, 255, 146', '90, 135, 255', '251, 243, 140'],
-  NEST: process.env.NEXT_PUBLIC_NEST || false, // 彩带开关
+  NEST: process.env.NEXT_PUBLIC_NEST || false// 彩带开关
 }
 
 module.exports = BLOG
