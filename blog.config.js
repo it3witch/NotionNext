1d591ea0d9d0702016b025d906a2308043234037
It3witch <xin2005629@163.com>
2025-10-12 15:18:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -80,6 +80,7 @@ const BLOG = {
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
   FIREWORKS_COLOR: ['255, 20, 97', '24, 255, 146', '90, 135, 255', '251, 243, 140'],
+  NEST: process.env.NEXT_PUBLIC_NEST || false, // 彩带开关
 }
 
 module.exports = BLOG
